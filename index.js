5abec0181587032df28982ed794195134bbaec55
Matt <1009003+tantaman@users.noreply.github.com>
2023-05-23 16:13:37 -0400

    pretty output

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import degit from 'degit';
+import chalk from 'chalk';
+const yellow = chalk.yellow;
+
+if (process.argv.length < 3) {
+  console.error('Please provide a target directory');
+  process.exit(1);
+}
 
 const emitter = degit('git@github.com:vlcn-io/vite-starter', {
 	cache: false,
@@ -11,6 +18,12 @@ emitter.on('info', info => {
 });
 
 emitter.clone(process.argv[2]).then(() => {
-	console.log('done');
+  console.log('');
+	console.log(`🎉 ${chalk.green.bold('Your project has been successfully created!')} 🎉`);
+  console.log(`\n${chalk.bold('Next steps:')}
+  - ${yellow.bold('cd')} ${process.argv[2]}
+  - ${yellow.bold('npm')} install
+  - ${yellow.bold('npm')} run dev
+`);
   process.exit(0);
 });
